9d126ea0b8158bc5d532d505961567e616fa1285
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-04 09:54:47 -0600

    Complete Lab including depth-first and breadth-first examples.

--- lib/tree.py
+++ lib/tree.py
@@ -2,5 +2,27 @@ class Tree:
   def __init__(self, root = None):
     self.root = root
 
+#use the depth-first approach
+#take a string as argument.
+#return the node with that value
+#if matching node is not found, return None
+#once pasisng, modify for breadth-first traversal (tests should still pass)
+  #Depth-first
+  # def get_element_by_id(self, id):
+  #   to_visit = [self.root]
+  #   while to_visit:
+  #     node = to_visit.pop()
+  #     if node['id'] == id:
+  #       return node
+  #     to_visit = node['children'] + to_visit
+  #   return None
+
+  #Breadth-first
   def get_element_by_id(self, id):
-    pass
+    to_visit = [self.root]
+    while to_visit:
+      node = to_visit.pop()
+      if node['id'] == id:
+        return node
+      to_visit = to_visit + node['children']
+    return None
